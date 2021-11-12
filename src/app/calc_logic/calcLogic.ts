4bee7a58a3c9b374fdb2cf5a0c9c9611f579ea59
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-11-12 00:57:24 -0800

    calc validation complete

--- src/app/calc_logic/calcLogic.ts
+++ src/app/calc_logic/calcLogic.ts
@@ -3,18 +3,19 @@ export type Calculation = (i1: string, i2: string) => number;
 export class calcLogic {
     divide(i1: string, i2: string) {
         if(parseInt(i1) === parseFloat(i1) &&  parseInt(i2) === parseFloat(i2)) return parseInt(i1) / parseInt(i2)
-        else return parseFloat(i1) / parseFloat(i2)
+        else return parseFloat((parseFloat(i1) / parseFloat(i2)).toFixed(2))
     }
     add(i1: string, i2: string) {
+        console.log(parseInt(i1) === parseFloat(i1) &&  parseInt(i2) === parseFloat(i2))
         if(parseInt(i1) === parseFloat(i1) &&  parseInt(i2) === parseFloat(i2)) return parseInt(i1) + parseInt(i2)
-        else return parseFloat(i1) + parseFloat(i2)
+        else return parseFloat((parseFloat(i1) + parseFloat(i2)).toFixed(2))
     }
     subtract(i1: string, i2: string) {
         if(parseInt(i1) === parseFloat(i1) &&  parseInt(i2) === parseFloat(i2)) return parseInt(i1) - parseInt(i2)
-        else return parseFloat(i1) - parseFloat(i2)
+        else return parseFloat((parseFloat(i1) - parseFloat(i2)).toFixed(2))
     }
     multiply(i1: string, i2: string) {
         if(parseInt(i1) === parseFloat(i1) &&  parseInt(i2) === parseFloat(i2)) return parseInt(i1) * parseInt(i2)
-        else return parseFloat(i1) * parseFloat(i2)
+        else return parseFloat((parseFloat(i1) * parseFloat(i2)).toFixed(2))
     }
 }
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,24 +6,43 @@ import { Calculation, calcLogic } from '../calc_logic/calcLogic';
                 <h1 id="calculator" class="p-1 leading-none text-2xl text-grey-darkest">Calculator</h1>  
                 <h1 class="text-red-300">{{errorMessage}}</h1>
               </div>
-              <div class="flex flex-col items-center justify-center bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+              <form #calcForm="ngForm" class="flex flex-col items-center justify-center bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <div>
-                <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" pattern="[0-9]*" [(ngModel)]="addinput1"> + <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" pattern="[0-9]*" [(ngModel)]="addinput2"> 
+                <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" pattern="[0-9.]*" minlength="1" [(ngModel)]="addinput1" name="addInput1" #addInput1="ngModel"> + <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" pattern="[0-9.]*" minlength="1" [(ngModel)]="addinput2" name="addInput2" #addInput2="ngModel"> 
                 <input class="mx-5 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" type='button' value="=" (click)=addClick()> 
-                {{addoutput}}</div> <br /> <br />
+                {{addoutput}}</div> <br /> 
+                <div *ngIf="addInput1?.errors?.['pattern']" class="errorbox">{{inputErrorPattern}}</div>
+                <div *ngIf="addInput2?.errors?.['pattern']" class="errorbox">{{inputErrorPattern}}</div>
+                <div *ngIf="addInput1?.errors?.['minlength']" class="errorbox">{{inputErrorMinlength}}</div>
+                <div *ngIf="addInput2?.errors?.['minlength']" class="errorbox">{{inputErrorMinlength}}</div>
+                <br />
                 <div>
-                <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" [(ngModel)]="subtractinput1"> - <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" [(ngModel)]="subtractinput2"> 
+                <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" pattern="[0-9.]*" minlength="1" [(ngModel)]="subtractinput1" name="subtractInput1" #subtractInput1="ngModel"> - <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" pattern="[0-9.]*" minlength="1" [(ngModel)]="subtractinput2" name="subtractInput2" #subtractInput2="ngModel"> 
                 <input class="mx-5 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" type='button' value="=" (click)=subtractClick()> 
-                {{subtractoutput}}</div> <br />  <br />
+                {{subtractoutput}}</div> <br /> 
+                <div *ngIf="subtractInput1?.errors?.['pattern']" class="errorbox">{{inputErrorPattern}}</div>
+                <div *ngIf="subtractInput2?.errors?.['pattern']" class="errorbox">{{inputErrorPattern}}</div>
+                <div *ngIf="subtractInput1?.errors?.['minlength']" class="errorbox">{{inputErrorMinlength}}</div>
+                <div *ngIf="subtractInput2?.errors?.['minlength']" class="errorbox">{{inputErrorMinlength}}</div>
+                <br />
                 <div>
-                <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" [(ngModel)]="divideinput1"> / <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" [(ngModel)]="divideinput2"> 
+                <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" pattern="[0-9.]*" minlength="1" [(ngModel)]="divideinput1" name="divideInput1" #divideInput1="ngModel"> / <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" pattern="[0-9.]*" minlength="1" [(ngModel)]="divideinput2" name="divideInput2" #divideInput2="ngModel"> 
                 <input  class="mx-5 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" type='button' value="=" (click)=divideClick()> 
-                {{divideoutput}}</div> <br /> <br />
+                {{divideoutput}}</div> 
+                <div *ngIf="divideInput1?.errors?.['pattern']" class="errorbox">{{inputErrorPattern}}</div>
+                <div *ngIf="divideInput2?.errors?.['pattern']" class="errorbox">{{inputErrorPattern}}</div>
+                <div *ngIf="divideInput1?.errors?.['minlength']" class="errorbox">{{inputErrorMinlength}}</div>
+                <div *ngIf="divideInput2?.errors?.['minlength']" class="errorbox">{{inputErrorMinlength}}</div>
+                <br /> <br />
                 <div>
-                <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" [(ngModel)]="multiplyinput1"> x <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" [(ngModel)]="multiplyinput2"> 
+                <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" pattern="[0-9.]*" minlength="1" [(ngModel)]="multiplyinput1" name="multiplyInput1" #multiplyInput1="ngModel"> x <input class="mx-5 shadow border border-grey-500 rounded w-20 py-2 px-3 text-gray-700 mb-3 focus:shadow-outline" pattern="[0-9.]*" minlength="1" [(ngModel)]="multiplyinput2" name="multiplyInput2" #multiplyInput2="ngModel"> 
                 <input class="mx-5 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" type='button' value="=" (click)=multiplyClick()> 
-                {{multiplyoutput}}</div>
-              </div>`,
+                {{multiplyoutput}}</div> <br />
+                <div *ngIf="multiplyInput1?.errors?.['pattern']" class="errorbox">{{inputErrorPattern}}</div>
+                <div *ngIf="multiplyInput2?.errors?.['pattern']" class="errorbox">{{inputErrorPattern}}</div>
+                <div *ngIf="multiplyInput1?.errors?.['minlength']" class="errorbox">{{inputErrorMinlength}}</div>
+                <div *ngIf="multiplyInput2?.errors?.['minlength']" class="errorbox">{{inputErrorMinlength}}</div>
+              </form>`,
   styleUrls: ['./calculator.component.scss'],
   providers: [calcLogic]
 })
@@ -32,10 +51,11 @@ export class CalculatorComponent implements OnInit {
   subtractinput1 = ''; subtractinput2 = ''; subtractoutput = '';
   divideinput1 = ''; divideinput2 = ''; divideoutput = '';
   multiplyinput1 = ''; multiplyinput2 = ''; multiplyoutput = '';
-  Add = () => this.addoutput = `${this.addinput1} + ${this.addinput2} = ${this.add(this.addinput1, this.addinput2).toFixed(2)}`
-  Subtract = () => this.subtractoutput = `${this.subtractinput1} - ${this.subtractinput2} = ${this.subtract(this.subtractinput1, this.subtractinput2).toFixed(2)}`
-  Divide = () => this.divideoutput = `${this.divideinput1} / ${this.divideinput2} = ${this.divide(this.divideinput1, this.divideinput2).toFixed(2)}`
-  Multiply = () => this.multiplyoutput = `${this.multiplyinput1} x ${this.multiplyinput2} = ${this.multiply(this.multiplyinput1, this.multiplyinput2).toFixed(2)}`
+  inputErrorPattern = 'please use only numbers'; inputErrorMinlength = 'please enter at least 1 number';
+  Add = () => this.addoutput = `${this.addinput1} + ${this.addinput2} = ${this.add(this.addinput1, this.addinput2)}`
+  Subtract = () => this.subtractoutput = `${this.subtractinput1} - ${this.subtractinput2} = ${this.subtract(this.subtractinput1, this.subtractinput2)}`
+  Divide = () => this.divideoutput = `${this.divideinput1} / ${this.divideinput2} = ${this.divide(this.divideinput1, this.divideinput2)}`
+  Multiply = () => this.multiplyoutput = `${this.multiplyinput1} x ${this.multiplyinput2} = ${this.multiply(this.multiplyinput1, this.multiplyinput2)}`
   add: Calculation; subtract: Calculation;
   divide: Calculation; multiply: Calculation;
   constructor(calcLogic: calcLogic) {
@@ -75,7 +95,7 @@ export class CalculatorComponent implements OnInit {
     else return true;
   }
   errorMsg = () => {
-    this.errorMessage = 'check that inputs are numbers'
+    this.errorMessage = 'ensure that you have entered a number for every field'
     setTimeout(() => {
       this.errorMessage = ''
     }, 5000)
